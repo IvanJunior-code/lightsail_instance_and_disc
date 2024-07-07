cc247e6e57d567a8b6dab7be18b8e150feb4a53b
Ivanzinho <ivan_junior_we@hotmail.com>
2024-07-06 22:09:06 -0300

    Adicionando dependencia do disco

--- instance.tf
+++ instance.tf
@@ -4,4 +4,5 @@ resource "aws_lightsail_instance" "lightsail_terraform" {
   blueprint_id      = var.blueprint_id
   bundle_id         = var.bundle_id
   key_pair_name     = var.key_pair_name
+  depends_on        = [aws_lightsail_disk.disk1]
 }
