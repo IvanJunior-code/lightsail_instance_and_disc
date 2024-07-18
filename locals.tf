abca8c5ae0c7ddd9b15865110261b164295add21
Ivanzinho <ivan_junior_we@hotmail.com>
2024-07-17 21:58:32 -0300

    Arquivo locals para definir tags padrões para os recursos.

---
+++ locals.tf
@@ -0,0 +1,8 @@
+locals {
+  management_tags = {
+    CreateBy    = "Terraform"
+    Environment = "Test"
+    Project     = "Terraform - Lightsail instance and disk"
+    Owner       = "Ivan"
+  }
+}
