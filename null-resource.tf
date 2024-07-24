1c9367fbf1c4ac44cc2dc9963b35bca5621450c8
Ivanzinho <ivan_junior_we@hotmail.com>
2024-07-23 21:28:27 -0300

    Criando recurso null_resource para acionar a execução do script após a criação da instância, disco e attachment do disco à instância.

---
+++ null-resource.tf
@@ -0,0 +1,26 @@
+resource "null_resource" "exec" {
+    provisioner "file" {
+    source = "script.sh"
+    destination = "/tmp/script.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo Exibindo filesystem",
+      "df -h",
+      "echo Exibindo tabela de partições e discos",
+      "sudo fdisk -l",
+      "echo Configurando permissão no script",
+      "chmod +x /tmp/script.sh",
+      "echo Executando script",
+      "sudo /tmp/script.sh"
+    ]
+  }
+
+  connection {
+    type = "ssh"
+    user = "ubuntu"
+    private_key = file(var.path_key_pair)
+    host = aws_lightsail_instance.lightsail_terraform.public_ip_address
+  }
+}
