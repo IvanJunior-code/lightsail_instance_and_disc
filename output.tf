741bd603b863d6ab4eb811e30865e13f96cb5b4b
Ivanzinho <ivan_junior_we@hotmail.com>
2024-07-23 21:53:24 -0300

    Ajuste no local da chave

--- output.tf
+++ output.tf
@@ -1,3 +1,3 @@
 output "connection_string" {
-  value = "ssh -i ~/.ssh/${var.key_pair_name}.pem ubuntu@${aws_lightsail_instance.lightsail_terraform.public_ip_address}"
+  value = "ssh -i ${var.path_key_pair} ubuntu@${aws_lightsail_instance.lightsail_terraform.public_ip_address}"
 }
