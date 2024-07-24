6b7bdb4f31cd08b099cdb9a13fc9daaed16f869d
Ivanzinho <ivan_junior_we@hotmail.com>
2024-07-23 21:33:18 -0300

    Ajuste no nome do recurso

--- disk-attachment.tf
+++ disk-attachment.tf
@@ -1,4 +1,4 @@
-resource "aws_lightsail_disk_attachment" "test" {
+resource "aws_lightsail_disk_attachment" "attachment" {
   disk_name     = aws_lightsail_disk.disk1.name
   instance_name = aws_lightsail_instance.lightsail_terraform.name
   disk_path     = "/dev/xvdf"
